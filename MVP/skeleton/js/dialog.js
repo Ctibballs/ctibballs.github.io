286b82bd649f4a944ab9eb71a6679dea1baca5d4
Ctibballs <christibballs1990@gmail.com>
2016-08-09 12:54:44 +1000

    seev

--- MVP/skeleton/js/dialog.js
+++ MVP/skeleton/js/dialog.js
@@ -2,13 +2,13 @@
 var modal = document.getElementById('sign-up');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var a = document.getElementByClassName("sign-up");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
+a.onclick = function() {
     modal.style.display = "block";
 }
 
